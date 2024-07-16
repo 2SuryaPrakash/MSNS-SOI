4da53948040e387ea51bfbf5dd94a0e860706ec4
Surya Prakash S <152375770+2SuryaPrakash@users.noreply.github.com>
2024-07-16 20:03:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var session = require('express-session');
 const bodyParser = require('body-parser');
 const schemas = require('./back/schemas');
 var db = schemas.db;
-const port = 3000;
+const port = 5001;
 
 // Importing routers
 var authRouter = require('./back/auth');
